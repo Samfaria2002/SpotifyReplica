dac742bf5de528ca3a06963eefe858ab95acd005
Samuel Faria Garcia <samuel.faria2002@hotmail.com>
2023-09-25 13:59:14 -0300

    Formatando grid base

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
+const path = require('path');
 
 gulp.task('sass', function () {
   return gulp
-    .src('D:/projetos/SpotifyReplica/static/sass/home.scss')
+    .src(path.join(__dirname, 'static/sass/home.scss'))
     .pipe(sass().on('error', sass.logError))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('D:/projetos/SpotifyReplica/static/css'));
+    .pipe(gulp.dest(path.join(__dirname, 'static/css')));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('D:/projetos/SpotifyReplica/static/sass/home.scss', gulp.series('sass'));
+  gulp.watch(path.join(__dirname, 'static/sass/home.scss'), gulp.series('sass'));
 });
 
 gulp.task('default', gulp.series('sass', 'watch'));
--- routes/rotas.py
+++ routes/rotas.py
@@ -3,4 +3,3 @@ from flask import Flask, Blueprint, render_template, url_for
 from flask_assets import Environment
 
 Bp = Blueprint('Bp', __name__)
-
