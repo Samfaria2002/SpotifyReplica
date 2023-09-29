11aa588474762ca05df6ac8e05866ffb1da4684c
Samuel Faria Garcia <samuel.faria2002@hotmail.com>
2023-09-29 11:15:50 -0300

    Estrutura básica para React

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from routes.rotas import Bp
 from flask_assets import Environment, Bundle
+import datetime
+
+x = datetime.datetime.now()
 
 app = Flask(__name__)
 app.register_blueprint(Bp)
@@ -13,5 +16,17 @@ assets.register('home', home)
 def home():
     return render_template('home.html', assets=assets)
 
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+ 
+    # Returning an api for showing in  reactjs
+    return {
+        'Name':"geek",
+        "Age":"22",
+        "Date": x,
+        "programming":"python"
+        }
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- routes/rotas.py
+++ routes/rotas.py
@@ -3,3 +3,7 @@ from flask import Flask, Blueprint, render_template, url_for
 from flask_assets import Environment
 
 Bp = Blueprint('Bp', __name__)
+
+@Bp.route('/home')
+def home():
+    return render_template('home.html')
