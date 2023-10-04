ec7f5fa5a6a5a48ae959329d720cfad98a9dfc87
Samuel Faria Garcia <samuel.faria2002@hotmail.com>
2023-10-04 16:43:00 -0300

    Registro de funcionário funcionando

--- main.py
+++ main.py
@@ -14,19 +14,5 @@ assets = Environment(app)
 cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), 'credentials', 'login-77993-firebase-adminsdk-72fip-42ebb7b404.json'))
 firebase_admin.initialize_app(cred)
 
-@app.route('/register', methods=['POST'])
-def register():
-    try:
-        email = request.json.get('email')
-        password = request.json.get('password')
-
-        user = auth.create_user(
-            email = email,
-            password = password
-        )
-        return jsonify({'message': 'Usuário registrado com sucesso!'})
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
 if __name__ == '__main__':
     app.run(debug=True)
--- routes/rotas.py
+++ routes/rotas.py
@@ -1,8 +1,25 @@
-from flask import Flask, Blueprint, render_template, url_for
+from flask import Flask, Blueprint, render_template, url_for, jsonify, request
 from flask_assets import Environment
+from firebase_admin import credentials, auth
 
 Bp = Blueprint('Bp', __name__)
 
 @Bp.route('/home')
 def home():
     return render_template('home.html')
+
+
+@Bp.route('/register', methods=['POST'])
+def register():
+    try:
+        data = request.json
+        email = data.get('email')
+        password = data.get('password')
+
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
+        return jsonify({'message': 'Usuário registrado com sucesso'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)})
